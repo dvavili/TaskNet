391303b5730c8801816e7363ba8572ab1dc5880f
DivyaVavili <divyavavili@cmu.edu>
2011-03-29 17:18:31 -0400

    Changed the package structure

---
+++ src/ds/android/tasknet/msgpasser/Message.java
@@ -0,0 +1,94 @@
+package ds.android.tasknet.msgpasser;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * @authors
+ * Divya Vavili - dvavili@andrew.cmu.edu
+ * Yash Pathak - ypathak@andrew.cmu.edu
+ *
+ */
+
+/**
+ * Message:
+ * Wraps the 'object' to be sent with destination, kind and id details
+ */
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public enum NormalMsgType { NORMAL, PROFILE_XCHG, TASK_ADV, PROFILE_UPDATE };
+
+    protected Serializable data;
+    protected String destination;
+    protected String kind;
+    protected String id;
+    protected String logMessage;
+    protected Boolean log;
+    protected NormalMsgType normalMsgType;
+
+    public Message(String dest, String kind, String id, Serializable data) {
+        this.data = data;
+        this.kind = kind;
+        this.destination = dest;
+        this.id = id;
+        log = false;
+        logMessage = "";
+        normalMsgType = NormalMsgType.NORMAL;
+    }
+
+    public void setNormalMsgType(NormalMsgType type){
+        normalMsgType = type;
+    }
+
+    public NormalMsgType getNormalMsgType(){
+        return normalMsgType;
+    }
+
+    public String getDest() {
+        return destination;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Serializable getData() {
+        return data;
+    }
+
+    public void setLogMessage(String status){
+        logMessage = status;
+    }
+
+    public String getLogMessage(){
+        return logMessage;
+    }
+
+    public boolean isToBeLogged(){
+        return log;
+    }
+
+    public void setToBeLogged(boolean flag){
+        log = flag;
+    }
+
+    public void setDest(String dest){
+        destination = dest;
+    }
+   
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+    }
+}
---
+++ src/ds/android/tasknet/task/Task.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ds.android.tasknet.task;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Divya_PKV
+ */
+public class Task implements Serializable{
+    private static final long serialVersionUID = 1L;
+    public Integer taskLoad;
+    public String taskId;
+
+    public Task(Integer load, String id){
+        taskId = id;
+        taskLoad = load;
+    }
+
+    public Integer getTaskLoad(){
+        return taskLoad;
+    }
+
+    public void setTaskLoad(Integer load){
+        taskLoad = load;
+    }
+}
